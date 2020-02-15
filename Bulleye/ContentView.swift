da1c05b8aaf679cec1f6c1a8582a43a133fce75b
daddyrusher <daddyrusher@gmail.com>
2020-02-15 19:44:47 +0500

    Initial Commit

---
+++ Bulleye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bulleye
+//
+//  Created by Максим Занин on 15.02.2020.
+//  Copyright © 2020 Максим Занин. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
